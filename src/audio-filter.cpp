6d6c7b6c84ff587b66556bf5ac087447c2fc0952
Roy Shilkrot <roy.shil@gmail.com>
2023-06-08 10:42:00 -0400

    circbuf added

--- src/audio-filter.cpp
+++ src/audio-filter.cpp
@@ -17,23 +17,34 @@
 
 #define MAX_PREPROC_CHANNELS 8
 
-/* Whisper.cpp constants */
-#define WHISPER_FRAME_SIZE 8000
+// 500ms buffer size
 #define BUFFER_SIZE_MSEC 500
+// at 16Khz, 500ms is 8000 samples
+#define WHISPER_FRAME_SIZE 8000
 
 #define S_cleanstream_DB "db"
 
 #define MT_ obs_module_text
+
+// Audio packet info
+struct cleanstream_audio_info {
+  uint32_t frames;
+  uint64_t timestamp;
+};
 
 struct cleanstream_data {
   obs_source_t *context;
   size_t channels;
   float multiple;
 
+  // How many input frames are needed for the next whisper frame
+  size_t frames;
+
   /* PCM buffers */
   float *copy_buffers[MAX_PREPROC_CHANNELS];
   float *segment_buffers[MAX_PREPROC_CHANNELS];
-	struct circlebuf input_buffers[MAX_PREPROC_CHANNELS];
+  struct circlebuf info_buffer;
+  struct circlebuf input_buffers[MAX_PREPROC_CHANNELS];
 
   /* Resampler */
   audio_resampler_t *resampler;
@@ -98,7 +109,7 @@ static void cleanstream_update(void *data, obs_data_t *s)
   gf->multiple = db_to_mul((float)val);
 
   uint32_t sample_rate = audio_output_get_sample_rate(obs_get_audio());
-  size_t frames = (size_t)sample_rate / (1000 / BUFFER_SIZE_MSEC);
+  gf->frames = (size_t)sample_rate / (1000 / BUFFER_SIZE_MSEC);
 
   struct resample_info src, dst;
   src.samples_per_sec = sample_rate;
@@ -113,13 +124,13 @@ static void cleanstream_update(void *data, obs_data_t *s)
   gf->resampler_back = audio_resampler_create(&src, &dst);
 
   /* One state for each channel (limit 2) */
-  gf->copy_buffers[0] = static_cast<float *>(bmalloc(frames * gf->channels * sizeof(float)));
+  gf->copy_buffers[0] = static_cast<float *>(bmalloc(gf->frames * gf->channels * sizeof(float)));
   gf->segment_buffers[0] =
     static_cast<float *>(bmalloc(WHISPER_FRAME_SIZE * gf->channels * sizeof(float)));
   for (size_t c = 1; c < gf->channels; ++c) {
-    gf->copy_buffers[c] = gf->copy_buffers[c - 1] + frames;
+    gf->copy_buffers[c] = gf->copy_buffers[c - 1] + gf->frames;
     gf->segment_buffers[c] = gf->segment_buffers[c - 1] + WHISPER_FRAME_SIZE;
-  	circlebuf_reserve(&gf->input_buffers[c - 1], frames * sizeof(float));
+    circlebuf_reserve(&gf->input_buffers[c - 1], gf->frames * sizeof(float));
   }
 }
 
@@ -132,30 +143,30 @@ static void *cleanstream_create(obs_data_t *settings, obs_source_t *filter)
   info("cleanstream_create");
 
   gf->whisper_params = whisper_full_default_params(WHISPER_SAMPLING_GREEDY);
-  gf->whisper_params.n_threads = std::min(4, (int32_t) std::thread::hardware_concurrency());
+  gf->whisper_params.n_threads = std::min(4, (int32_t)std::thread::hardware_concurrency());
 
   cleanstream_update(gf, settings);
   return gf;
 }
 
-static std::string to_timestamp(int64_t t) {
-    int64_t sec = t/100;
-    int64_t msec = t - sec*100;
-    int64_t min = sec/60;
-    sec = sec - min*60;
+static std::string to_timestamp(int64_t t)
+{
+  int64_t sec = t / 100;
+  int64_t msec = t - sec * 100;
+  int64_t min = sec / 60;
+  sec = sec - min * 60;
 
-    char buf[32];
-    snprintf(buf, sizeof(buf), "%02d:%02d.%03d", (int) min, (int) sec, (int) msec);
+  char buf[32];
+  snprintf(buf, sizeof(buf), "%02d:%02d.%03d", (int)min, (int)sec, (int)msec);
 
-    return std::string(buf);
+  return std::string(buf);
 }
 
 static void run_whisper_inference(struct cleanstream_data *gf, const float *pcm32f_data,
                                   size_t pcm32f_size)
 {
-  info("%s: processing %d samples, %.3f sec), %d threads",
-        __func__, int(pcm32f_size), float(pcm32f_size)/WHISPER_SAMPLE_RATE,
-        gf->whisper_params.n_threads);
+  info("%s: processing %d samples, %.3f sec), %d threads", __func__, int(pcm32f_size),
+       float(pcm32f_size) / WHISPER_SAMPLE_RATE, gf->whisper_params.n_threads);
 
   // run the inference
   if (whisper_full(gf->whisper_context, gf->whisper_params, pcm32f_data, pcm32f_size) != 0) {
@@ -173,27 +184,50 @@ static void run_whisper_inference(struct cleanstream_data *gf, const float *pcm3
   }
 }
 
-static struct obs_audio_data *cleanstream_filter_audio(void *data, struct obs_audio_data *audio)
+static void process_audio_from_buffer(struct cleanstream_data *gf)
 {
-  struct cleanstream_data *gf = static_cast<struct cleanstream_data *>(data);
-  const size_t channels = gf->channels;
-  float **adata = (float **)audio->data;
+  /* Pop from input circlebuf */
+  for (size_t i = 0; i < gf->channels; i++)
+    circlebuf_pop_front(&gf->input_buffers[i], gf->copy_buffers[i], gf->frames * sizeof(float));
 
   // resample to 16kHz
   float *output[MAX_PREPROC_CHANNELS];
   uint32_t out_frames;
   uint64_t ts_offset;
   audio_resampler_resample(gf->resampler, (uint8_t **)output, &out_frames, &ts_offset,
-                           (const uint8_t **)adata, audio->frames);
+                           (const uint8_t **)gf->copy_buffers, (uint32_t)gf->frames);
 
-  info("%d channels, %d frames, %f ms", (int)channels, (int)out_frames, (float)out_frames / WHISPER_SAMPLE_RATE * 1000.0f);
+  info("%d channels, %d frames, %f ms", (int)gf->channels, (int)out_frames,
+       (float)out_frames / WHISPER_SAMPLE_RATE * 1000.0f);
 
   // run the inference
   run_whisper_inference(gf, output[0], out_frames);
+}
 
-  // // resample back to original sample rate
-  // audio_resampler_resample(gf->resampler_back, (uint8_t **)output, &out_frames, &ts_offset,
-  //                          (const uint8_t **)gf->segment_buffers, WHISPER_FRAME_SIZE);
+static struct obs_audio_data *cleanstream_filter_audio(void *data, struct obs_audio_data *audio)
+{
+  struct cleanstream_data *gf = static_cast<struct cleanstream_data *>(data);
+  size_t segment_size = gf->frames * sizeof(float);
+
+  /* -----------------------------------------------
+	 * push audio packet info (timestamp/frame count) to info circlebuf */
+  // struct cleanstream_audio_info info = {0};
+  // info.frames = audio->frames;       // number of frames in this packet
+  // info.timestamp = audio->timestamp; // timestamp of this packet
+  // circlebuf_push_back(&gf->info_buffer, &info, sizeof(info));
+
+  /* -----------------------------------------------
+	 * push back current audio data to input circlebuf */
+  for (size_t i = 0; i < gf->channels; i++) {
+    circlebuf_push_back(&gf->input_buffers[i], audio->data[i], audio->frames * sizeof(float));
+  }
+
+  /* -----------------------------------------------
+	 * pop/process segments if they are big enough for Whisper, push back to output circlebuf */
+  while (gf->input_buffers[0].size >= segment_size) {
+    info("found %d frames in input buffer, processing", (int)(gf->input_buffers[0].size));
+    process_audio_from_buffer(gf);
+  }
 
   return audio;
 }
